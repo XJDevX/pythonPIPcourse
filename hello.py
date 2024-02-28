e3ea08d9a3d4f63d44b7f92153270559d1e0dff4
jxdeveloper <julianparrales19@gmail.com>
2024-02-28 18:35:56 -0500

    Agregado un nuevo texto al codigo

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 #<<--Programando en Python con PIP y terminal de comandos-->>#
 message = "Hola, estoy programando en Python!"
+message2 = "Hoy es 28 de Febrero del 2024"
 
-print(message)
+print(message, message2)
