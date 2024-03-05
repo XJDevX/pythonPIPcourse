749d23323e6839450baf70b339b6346bead0521a
jxdeveloper <julianparrales19@gmail.com>
2024-03-05 06:15:58 -0500

    Agregada informacion a 'webserver/' y notes.txt

--- webserver/main.py
+++ webserver/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [1,2,3,4,5,6,7,8,9,10]
+
+@app.get("/contacts", response_class = HTMLResponse)
+def get_list():
+    return """
+        <h1>Hola soy una pagina</h1>
+        <p>Hola soy un parrafo</p>
+    """
 
 def run():
     store.getCategories()
